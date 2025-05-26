a42ada1522fb406baf5d2db6f58fe3dc4eb53fbc
Thalisson Vieira <32040335+Thalinhos@users.noreply.github.com>
2025-05-26 16:28:31 -0300

    Update main.py

--- docker-infra-loadBalancer-2/app/main.py
+++ docker-infra-loadBalancer-2/app/main.py
@@ -1,19 +1,27 @@
-import socket
 from flask import Flask
-import os
-import psycopg2
+import socket
+from psycopg2 import pool
 
 app = Flask(__name__)
 
+# Criando o pool na inicialização do app
+db_pool = pool.SimpleConnectionPool(
+    1, 20,  # minconn, maxconn
+    host="db",
+    database="meubanco",
+    user="meuuser",
+    password="minhasenha"
+)
+
 @app.route("/")
 def hello():
-    conn = psycopg2.connect(
-        host="db",
-        database="meubanco",
-        user="meuuser",
-        password="minhasenha"
-    )
-    return f"Hello from Flask with PostgreSQL! ->  {socket.gethostname()}" #added to show the hostname of the container
+    conn = db_pool.getconn()
+    try:
+        # Aqui você faria sua lógica com o banco
+        # Exemplo: cursor = conn.cursor() ...
+        return f"Hello from Flask with PostgreSQL! ->  {socket.gethostname()}"
+    finally:
+        db_pool.putconn(conn)  # devolve a conexão ao pool
 
 if __name__ == "__main__":
     app.run(host="0.0.0.0", port=5000)
